3b99109985e50ac72555f16df24d935f639d4c0e
Alex Kangas <alex.kangas.5644@student.uu.se>
2020-02-11 15:40:37 +0100

    Add manager and participant directories

---
+++ src/participant/public/js/vue_script.js
@@ -0,0 +1,10 @@
+'use strict';
+const socket = io();
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
