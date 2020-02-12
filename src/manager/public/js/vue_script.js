1ed30ace54541299857e95a1021e11b4de795f50
Ansar SIddiqui <ansar.99@hotmail.com>
2020-02-12 14:16:45 +0100

    unmatching implemented

--- src/manager/public/js/vue_script.js
+++ src/manager/public/js/vue_script.js
@@ -1,10 +1,34 @@
 'use strict';
 const socket = io();
 
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-}
 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-}
+const vm= new Vue({
+    el:"#main",
+    data:{
+
+    },
+    methods:{
+        unMatch: function(event){
+
+            let newrow= document.createElement("tr");
+            let newtd= document.createElement("td");
+            newtd.className ="unMatched";
+            let p = document.createElement("p");
+
+            let id = event.currentTarget.id;
+
+            let text = document.createTextNode(id);
+            p.appendChild(text);
+            newtd.appendChild(p);
+            newrow.appendChild(newtd);
+            let table= document.getElementById("myTable");
+            table.appendChild(newrow);
+            let elem = document.getElementById(id);
+            elem.parentNode.removeChild(elem);
+
+
+
+
+        }
+    }
+})
