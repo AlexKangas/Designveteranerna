bbd8ec1de73d9428e84ced4a55e8e5578dc7b240
Ansar SIddiqui <ansar.99@hotmail.com>
2020-02-15 17:45:38 +0100

    Edit functionality almost done

--- src/manager/public/js/vue_script.js
+++ src/manager/public/js/vue_script.js
@@ -5,37 +5,153 @@ const socket = io();
 const vm= new Vue({
     el:"#main",
     data:{
+        selected: [],
 
     },
     methods:{
+
         unMatch: function(event){
-            //create necessary elements and fetch unmatchtable
-            let matchtable = event.currentTarget.parentNode.parentNode.parentNode.parentNode.id;
-            if(matchtable === "matchTable"){
-                let uTable= document.getElementById("unMatchTable");
-                let newrow= document.createElement("tr");
-                let newtd= document.createElement("td");
-                let p = document.createElement("p");
+            let children = document.getElementById("matchTable").childNodes;
+            let bool = false;
+            for(let i = 0; i < children.length; i++){
+                if(children[i] === event.currentTarget){
+                    bool = true;
+                }
+            }
+            if(event.currentTarget.parentNode.parentNode == document.getElementById("matchTable") || bool){
 
+                let newrow1= document.createElement("tr");
+                let newtd1= document.createElement("td");
 
-                let id = event.currentTarget.id;
-                let elem= document.getElementById(id);
-                let cName= elem.parentNode.className;
-                let text = document.createTextNode(id);
-                newtd.className= cName;
+                let newrow2= document.createElement("tr");
+                let newtd2= document.createElement("td");
 
-                p.appendChild(text);
-                newtd.appendChild(p);
-                newrow.appendChild(newtd);
-                uTable.appendChild(newrow);
+                let row = event.currentTarget;
 
-                elem.parentNode.removeChild(elem);
+                let person1 = row.cells[0];
+                let person2= row.cells[1];
+
+                newtd1.id = person1.id;
+                newtd1.className=person1.className;
+
+                newtd2.id = person2.id;
+                newtd2.className = person2.className;
+
+                let text1 = document.createTextNode(newtd1.id);
+                let text2 = document.createTextNode(newtd2.id);
+
+                newrow1.addEventListener("click",this.unMatch);
+                newrow2.addEventListener("click",this.unMatch);
+
+                newtd1.appendChild(text1);
+                newrow1.appendChild(newtd1);
+
+                newtd2.appendChild(text2);
+                newrow2.appendChild(newtd2);
+
+                let uTable = document.getElementById("unMatchTable");
+                uTable.appendChild(newrow1);
+                uTable.appendChild(newrow2);
+
+                row.parentNode.removeChild(row);
 
             }
             else{
-                alert("Flyttar till matchtable");
+                let target = event.currentTarget;
+                let child = event.currentTarget.firstChild;
+
+                if((this.selected.includes(child.id) && child.className=="menColumn") || (this.selected.includes(child.id) && child.className == "womenColumn")){
+
+                    for(var i = 0; i < this.selected.length; i++ ){
+                        if(this.selected[i] === child.id){
+                            this.selected.splice(i,1);
+                            target.className = "";
+                        }
+                    }
+                }
+                else if(this.selected.length < 2){
+                    target.className="selected";
+                    this.selected.push(child.id);
+
+                }else{
+                    alert("don't allow to mark more than two person");
+                }
+
+            }
+        },
+        rematch: function(){
+            let person1 = document.getElementById(this.selected[0]);
+            let person2 = document.getElementById(this.selected[1]);
+            let mTable = document.getElementById("matchTable");
+
+
+            if(this.selected.length == 2 &&( (person1.className=="menColumn" && person2.className =="womenColumn") || (person1.className=="womenColumn" && person2.className=="menColumn" ))){
+
+
+                let newrow= document.createElement("tr");
+                let newtd1= document.createElement("td");
+                let newtd2= document.createElement("td");
+
+                if(person1.className=="menColumn"){
+                    //Lägger till paret i matchtable
+
+                    let text1 = document.createTextNode(person1.id);
+                    newtd1.id = person1.id;
+                    newtd1.className = person1.className;
+                    newtd1.appendChild(text1);
+                    newrow.appendChild(newtd1);
+
+                    let text2 = document.createTextNode(person2.id);
+                    newtd2.id = person2.id;
+                    newtd2.className = person2.className;
+                    newtd2.appendChild(text2);
+                    newrow.appendChild(newtd2)
+                    newrow.addEventListener("click",this.unMatch);;
+
+                    mTable.appendChild(newrow);
+
+                    //Tar bort dem från unMatchTable och selected
+                    let toRemove1 = person1.parentNode;
+                    let toRemove2 = person2.parentNode;
+
+                    toRemove1.parentNode.removeChild(toRemove1);
+                    toRemove2.parentNode.removeChild(toRemove2);
+                    this.selected.pop();
+                    this.selected.pop();
+
+
+                }
+                else{
+                    //Lägger till paret i matchtable
+                    let text2 = document.createTextNode(person2.id);
+                    newtd2.id = person2.id;
+                    newtd2.className = person2.className;
+                    newtd2.appendChild(text2);
+                    newrow.appendChild(newtd2);
+
+                    let text1 = document.createTextNode(person1.id);
+                    newtd1.id = person1.id;
+                    newtd1.className = person1.className;
+                    newtd1.appendChild(text1);
+                    newrow.appendChild(newtd1);
+                    newrow.addEventListener("click",this.unMatch);
+
+                    mTable.appendChild(newrow);
+
+                    //Tar bort dem från unMatchTable och selected
+                    let toRemove1 = person1.parentNode;
+                    let toRemove2 = person2.parentNode;
+                    toRemove1.parentNode.removeChild(toRemove1);
+                    toRemove2.parentNode.removeChild(toRemove2);
+                    this.selected.pop();
+                    this.selected.pop();
+                }
+            }
+            else{
+                alert("Select two persons (one man and one woman)");
             }
         }
-
     }
+
+
 })
