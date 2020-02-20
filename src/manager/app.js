b41c5aa7e084c03b2d491da7f7d209a4dbf4cec3
Ansar SIddiqui <ansar.99@hotmail.com>
2020-02-20 20:45:21 +0100

    added start page and altered css and html for manager page

--- src/manager/app.js
+++ src/manager/app.js
@@ -69,6 +69,10 @@ app.get('/participant_start', function(req, res) {
     res.sendFile(path.join(__dirname, 'views/participant_start.html'));
 
 });
+app.get('/manager_start', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/manager_start.html'));
+
+});
 
 
 
